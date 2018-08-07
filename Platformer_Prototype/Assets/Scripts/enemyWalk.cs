30905f5602c2397a652a83c2316fc874a10592e2
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-08-07 12:18:41 +1200

    turret now work in my scene,

--- Platformer_Prototype/Assets/Scripts/enemyWalk.cs
+++ Platformer_Prototype/Assets/Scripts/enemyWalk.cs
@@ -5,46 +5,85 @@ using UnityEngine.AI;
 
 public class enemyWalk : MonoBehaviour {
     public GameObject des;
+    public GameObject argoTo;
     public float speed;
     public float dis;
     NavMeshAgent agent;
+   
+    public bool argro = false;
 	// Use this for initialization
 	void Start () {
+       
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (des != null)
+        if (argro == true)
         {
-            // if (Vector3.Distance(transform.position, des.transform.position) < 1.0f)
-            //{
-            //    Destroy(gameObject);
+           
 
-            //}
-            agent.SetDestination(des.transform.position);
-
-            Vector3 raycastDir = des.transform.position - transform.position;
-            raycastDir.y = raycastDir.y + 1.05f;
+            Vector3 raycastDir = argoTo.transform.position - transform.position;
+            //raycastDir.y = raycastDir.y + 1.05f;
 
             RaycastHit hit;
             Ray workingray = new Ray(transform.position, raycastDir);
-
             if (Physics.Raycast(workingray, out hit))
             {
-                if (hit.distance <= dis)
+                if (hit.distance >= 3)
                 {
-                    if (hit.transform.tag == "Goal")
-                    {
-                        hit.transform.GetComponent<endzone>().health--;
-                        Destroy(gameObject);
-                    }
-                    //print("check point");
+                    agent.SetDestination(argoTo.transform.position);
+
                 }
+                else
+                {
+                    //attack;
+                }
+
             }
-            raycastDir = raycastDir.normalized;
-            Debug.DrawRay(transform.position, raycastDir * dis, Color.blue);
+
         }
-	}
+        else
+        {
+            if (des != null)
+            {
+                // if (Vector3.Distance(transform.position, des.transform.position) < 1.0f)
+                //{
+                //    Destroy(gameObject);
+
+                //}
+                agent.SetDestination(des.transform.position);
+
+                Vector3 raycastDir = des.transform.position - transform.position;
+                raycastDir.y = raycastDir.y + 1.05f;
+
+                RaycastHit hit;
+                Ray workingray = new Ray(transform.position, raycastDir);
+
+                if (Physics.Raycast(workingray, out hit))
+                {
+                    if (hit.distance <= dis)
+                    {
+                        if (hit.transform.tag == "Goal")
+                        {
+                            hit.transform.GetComponent<endzone>().health--;
+                            Destroy(gameObject);
+                        }
+                        //print("check point");
+                    }
+                }
+                raycastDir = raycastDir.normalized;
+                Debug.DrawRay(transform.position, raycastDir * dis, Color.blue);
+            }
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                argro = true;
+                agent.stoppingDistance = 2;
+            }
+        }
+    }
+
+   
 }
---
+++ Platformer_Prototype/Assets/turretFinder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretFinder : MonoBehaviour {
+    SphereCollider argoRange;
+  
+	// Use this for initialization
+	void Start () {
+        argoRange = GetComponent<SphereCollider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("gi");
+        if (GetComponent<enemyWalk>().argro == true)
+        {
+            //find out which is more of a problem
+        }
+        else
+        {
+            if (col.transform.tag == "Turret")
+            {
+                print("hti");
+                GetComponent<enemyWalk>().argro = true;
+                GetComponent<enemyWalk>().argoTo = col.gameObject;
+            }
+        }
+
+
+    }
+}
